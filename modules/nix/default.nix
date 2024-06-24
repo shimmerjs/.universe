8b5c930c79de0914f2546d8a416bd3af3d0d2d93
alex weidner <shimmerjs@dpu.sh>
2024-06-24 11:46:29 -0400

    tweak nix settings for caching + disk space

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -3,6 +3,24 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Trade disk space for cached builds
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+
+  # Get disk space back, UK style, apparently
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Hour = 3;
+      Minute = 0;
+      Weekday = 3;
+    };
+  };
+
   nixpkgs.config = {
     allowUnfree = true; # SORRY
   };
