fe151db695d7f365e6be209872a0140853616e99
alex weidner <shimmerjs@dpu.sh>
2024-06-24 11:06:05 -0400

    update install script based on macos installer being multi-user by default now

--- hack/install-nix.sh
+++ hack/install-nix.sh
@@ -6,13 +6,4 @@ source $(dirname ${BASH_SOURCE[0]})/lib.sh
 
 echo "installing nix@$NIX_VERSION"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  FLAGS="--daemon"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  FLAGS="--darwin-use-unencrypted-nix-store-volume"
-else
-  echo "man, idk"
-  exit 1
-fi
-
-sh <(curl -L https://releases.nixos.org/nix/nix-$NIX_VERSION/install) "$FLAGS"
+sh <(curl -L https://releases.nixos.org/nix/nix-$NIX_VERSION/install) "$@"
