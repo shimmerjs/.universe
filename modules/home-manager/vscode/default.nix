bd5bc670e4330ce32b20037e3f4fa952c6b7a262
alex weidner <shimmerjs@dpu.sh>
2024-07-16 20:24:19 -0400

    vscode config tweaks + comment

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -2,7 +2,13 @@
 {
   programs.vscode = with pkgs; {
     enable = true;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    mutableExtensionsDir = false;
     extensions = with vscode-extensions; [
+      # Pull remote-ssh extension from nixpkgs to pick up special patches that
+      # fix interactions with the remote development server the extension tries
+      # to set up.
       ms-vscode-remote.remote-ssh
     ] ++ vscode-utils.extensionsFromVscodeMarketplace (import ./extensions.nix).extensions;
     userSettings = import ./user-settings.nix;
