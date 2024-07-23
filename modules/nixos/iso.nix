f0e05355a1c0eece3d1abeb76bf6d0d6287b8bb3
alex weidner <shimmerjs@dpu.sh>
2024-07-22 22:59:59 -0400

    proper string interpolation for path in iso.nix

--- modules/nixos/iso.nix
+++ modules/nixos/iso.nix
@@ -3,16 +3,16 @@
 { config, pkgs, hostname, ... }:
 {
   imports = [
-    ${pkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix
+    "${pkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
 
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
-    ${pkgs}/nixos/modules/installer/cd-dvd/channel.nix
+    "${pkgs}/nixos/modules/installer/cd-dvd/channel.nix"
 
     # passwordless root on image
     ./nixos/security/sudo.nix
     # root ssh key setup for installation
-    ./nixos/ssh/root.nix
+    # ./nixos/ssh/root.nix
     # include tailscale config so that we can register the 
     # node with our mesh during bootstrapping
     ./nixos/networking
