03906f47f174c55c8a0516ec91f807e2c51545cd
alex weidner <shimmerjs@dpu.sh>
2024-08-08 23:38:28 -0400

    rpi: remove configs that are now covered by raspberry-pi-nix

--- modules/nixos/raspberry-pi-4b.nix
+++ modules/nixos/raspberry-pi-4b.nix
@@ -12,19 +12,8 @@
 
   # Hardware configuration originally generated on a RPi 4b and inlined here.
   boot.initrd.availableKernelModules = [ "xhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
 
-  swapDevices = [ ];
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
-
-  # Taken from https://nixos.wiki/wiki/NixOS_on_ARM#Installation
-  # NixOS wants to enable GRUB by default
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.interfaces.eth0.useDHCP = true;
 }
