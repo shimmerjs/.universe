8dd888f543861ec2442fa7efc32a0e753b257d41
alex weidner <shimmerjs@dpu.sh>
2024-08-25 19:51:41 -0400

    mother: setup tailscale

--- hosts/mother.nix
+++ hosts/mother.nix
@@ -6,6 +6,17 @@
   homie = import ../homies/shimmerjs;
 
   systemConfig = { user, ... }: {
+    imports = [
+      ../modules/darwin/tailscale.nix
+    ];
+
+    # TODO: fetch additional network services before applying
+    networking.knownNetworkServices = [
+      "Ethernet"
+      "Thunderbolt Bridge"
+      "Wi-Fi"
+    ];
+
     users.users.${user}.openssh.authorizedKeys = {
       keyFiles = [
         ../homies/shimmerjs/shimmerjs.pub
