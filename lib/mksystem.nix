ddf046d622faf4f1d0d9ae2e99a3cc6b04017e6a
alex weidner <shimmerjs@dpu.sh>
2025-12-31 01:59:59 -0500

    remove mac-utils bits now that home-manager handles copying apps directly

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -70,11 +70,6 @@ systemFn rec {
     # Load homie system configuration if present.
     homieOSConfig.systemConfig or { }
   ]
-  ++ (lib.optionals isDarwin [
-    # # Ensure that apps installed via nix-darwin show up in Spotlight and the
-    # # Applications folder.
-    # inputs.mac-app-util.darwinModules.default
-  ])
   ++ (lib.optionals (hostConfig ? "diskConfig") [
     inputs.disko.nixosModules.disko
     hostConfig.diskConfig
